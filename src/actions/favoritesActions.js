0779ffa61df0a2cf46b4f60443526890cfdc0264
Aysegul BAS <aysegul.bas@arkas.com>
2023-05-24 16:41:56 +0300

    ok

--- src/actions/favoritesActions.js
+++ src/actions/favoritesActions.js
@@ -2,14 +2,13 @@ export const TOGGLE_FAVORITES = "TOGGLE_FAVORITES";
 export const ADD_FAVORITE = "ADD_FAVORITE";
 export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
 
-export const toggleFavorites = () => {
-  return ({ type: TOGGLE_FAVORITES });
-}
-
 export const addFavorite = (movie) => {
-  return ({ type: ADD_FAVORITE, payload: movie });
-}
+  return { type: ADD_FAVORITE, payload: movie };
+};
+export const toggleFavorites = () => {
+  return { type: TOGGLE_FAVORITES };
+};
 
 export const removeFavorite = (id) => {
-  return ({ type: REMOVE_FAVORITE, payload: id });
-}
+  return { type: REMOVE_FAVORITE, payload: id };
+};
